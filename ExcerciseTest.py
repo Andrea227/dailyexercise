df80a605de837a93b99f50126e5dd352b8e7b1f6
Andrea227 <andreayangsh@outlook.com>
2019-08-28 19:16:47 -0400

    Test 2

--- ExcerciseTest.py
+++ ExcerciseTest.py
@@ -1,5 +1,10 @@
-def you(x):
-    a = x+1
-    return a
+import numpy as np
+import matplotlib.pyplot as pt
 
-print(you(3))
+x = np.arange(0, 360)
+y = np.sin(x * np.pi / 180.0)
+pt.plot(x, y)
+pt.xlim(0, 360)
+pt.ylim(-1.2, 1.2)
+pt.title("SIN function")
+pt.show()
