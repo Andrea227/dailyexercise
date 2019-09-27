58d898953bccb9cf5faed053bb3398af35769849
Andrea227 <andreayangsh@outlook.com>
2019-09-27 19:25:54 -0400

    HW3-2.4-5

--- HW3/HW3-2.3-13.py
+++ HW3/HW3-2.3-13.py
@@ -3,8 +3,8 @@ from scipy.misc import derivative
 # definition of the input
 p0 = float(eval(input("Enter p0: ")))
 p1 = float(eval(input("Enter p1: ")))
-TOL = float(eval(input("Enter TOL: ")))
-maxi = float(input("Enter Iteration: "))
+TOL = float(10**(-6))
+maxi = float(100)
 p2 = float((p0 + p1) / 2)
 
 f = lambda x: 230 * (x ** 4) + 18 * (x ** 3) + 9 * (x ** 2) - 221 * x - 9
@@ -16,10 +16,12 @@ def secant(fn, p0, p1, TOL, N0):
     i = 2
     q0 = fn(p0)
     q1 = fn(p1)
+    b = []
     while i <= N0:
         p = p1 - q1 * (p1 - p0) / (q1 - q0)
+        b += [p]
         if abs(p - p1) < TOL:
-            return print("p =", "%.9f" % p)
+            return print("Secant method gave us %s as iteration %d" % (b, i))
             break
         else:
             p0 = p1
@@ -36,10 +38,12 @@ def failpo(fn, p0, p1, TOL, N0):
     i = 2
     q0 = fn(p0)
     q1 = fn(p1)
+    b = []
     while i <= N0:
         p = p1 - q1 * (p1 - p0) / (q1 - q0)
+        b += [p]
         if abs(p - p1) < TOL:
-            return print("p = ", "%.9f" % p)
+            return print("Falsi method gave us %s as iteration %d" % (b, i))
             break
         else:
             q = fn(p)
@@ -56,10 +60,12 @@ def failpo(fn, p0, p1, TOL, N0):
 # Newton's Method 1
 def newt1(fn, p3, TOL, N0):
     i = 1
+    b = []
     while i <= N0:
         p = p3 - fn(p3) / (derivative(fn, p3))
+        b += [p]
         if abs(p - p3) < TOL:
-            return print("p = ", "%.9f" % p)
+            return print("Newt method with scipy gave us %s as iteration %d" % (b, i))
             break
         else:
             p3 = p
@@ -71,10 +77,12 @@ def newt1(fn, p3, TOL, N0):
 # Newton's Method 2
 def newt2(fn, fn2, p3, TOL, N0):
     i = 1
+    b = []
     while i <= N0:
         p = p3 - (fn(p3) / fn2(p3))
+        b += [p]
         if abs(p - p3) < TOL:
-            return print("p = ", "%.9f" % p)
+            return print("Newt method with writen f' gave us %s as iteration %d" % (b, i))
             break
         else:
             p3 = p
