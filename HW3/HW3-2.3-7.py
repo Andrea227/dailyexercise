837ef7816f1d744ee4bf3faec9d3037e6f0d35cf
Andrea227 <andreayangsh@outlook.com>
2019-09-27 18:28:44 -0400

    HW3-2.4-5

--- HW3/HW3-2.3-7.py
+++ HW3/HW3-2.3-7.py
@@ -7,10 +7,10 @@ p1 = float(eval(input("Enter p1: ")))
 TOL = float(eval(input("Enter TOL: ")))
 maxi = float(input("Enter Iteration: "))
 
-# f = lambda x: (x ** 3) - 2 * (x ** 2) - 5
+f = lambda x: (x ** 3) - 2 * (x ** 2) - 5
 # f = lambda x: (x ** 3) + 3 * (x ** 2) - 1
 # f = lambda x: x - math.cos(x)
-f = lambda x: x - 0.8 - 0.2 * math.sin(x)
+# f = lambda x: x - 0.8 - 0.2 * math.sin(x)
 
 
 # secant algorithm
@@ -18,10 +18,12 @@ def secant(fn, p0, p1, TOL, maxi):
     i = 2
     q0 = fn(p0)
     q1 = fn(p1)
+    b = []
     while i <= maxi:
         p = p1 - q1 * (p1 - p0) / (q1 - q0)
+        b += [p]
         if abs(p - p1) < TOL:
-            return print("p =", "%.6f" % p)
+            return print("p = %s with iteration %d" % (str(b), i))
             sys.exit()
         else:
             p0 = p1
@@ -34,3 +36,4 @@ def secant(fn, p0, p1, TOL, maxi):
 
 
 secant(f, p0, p1, TOL, maxi)
+
