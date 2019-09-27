86c7140c60e312164a43917330b9b977954ee329
Andrea227 <andreayangsh@outlook.com>
2019-09-27 19:38:33 -0400

    HW3-2.4-5

--- HW3/HW3-2.3-16.py
+++ HW3/HW3-2.3-16.py
@@ -3,20 +3,22 @@ from math import cos
 from math import sin
 
 x = [-100, -50, -25, 25, 50, 100]
-TOL = 10 ** (-5)
-N1 = 100
-f = lambda x: x ** 2 - 10 * cos(x)
-f1 = lambda x: 2 * x + 10 * sin(x)
+TOL = float(10 ** (-5))
+N1 = float(100)
+f = lambda x: x ** float(2) - float(10) * cos(x)
+f1 = lambda x: float(2) * x + float(10) * sin(x)
 
 
 # Newton's Method 1
 def newt1(fn, p3, TOL, N0):
     i = 1
     a = p3
+    b = []
     while i <= N0:
         p = p3 - fn(p3) / (derivative(fn, p3, dx=1e-6))
+        b += [p]
         if abs(p - p3) < TOL:
-            return print("p1 = %.6f for p0 = %d" % (p, a))
+            return print("Newton's method with scipy is %s for p0 = %d" % (str(b), a))
             break
         else:
             p3 = p
@@ -29,10 +31,12 @@ def newt1(fn, p3, TOL, N0):
 def newt2(fn, fn2, p3, TOL, N0):
     i = 1
     a = p3
+    b = []
     while i <= N0:
         p = p3 - fn(p3) / fn2(p3)
+        b += [p]
         if abs(p - p3) < TOL:
-            return print("p2 = %.6f for p0 = %d" % (p, a))
+            return print("Newton's method with written f' is %s for p0 = %d" % (str(b), a))
             break
         else:
             p3 = p
@@ -42,5 +46,5 @@ def newt2(fn, fn2, p3, TOL, N0):
 
 
 for i in x:
-    newt1(f, i, TOL, N1)
-    newt2(f, f1, i, TOL, N1)
+    newt1(f, float(i), TOL, N1)
+    newt2(f, f1, float(i), TOL, N1)
