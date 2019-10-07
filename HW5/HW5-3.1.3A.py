e00a7c2a6cf49c0d6f555b53a4e1d5027d41bef7
Andrea227 <andreayangsh@outlook.com>
2019-10-07 16:46:20 +0800

    HW5-3.1

---
+++ HW5/HW5-3.1.3A.py
@@ -0,0 +1,21 @@
+import scipy.optimize as opt
+import scipy
+from numpy import *
+from numpy import sin, cos
+import math
+
+xp = float(input("What's your approximation point: "))
+degree = int(input("What's the degree of approximation poly?: "))
+xlist = [float(input('The first x0 is: '))]
+b = 1
+while b <= int(degree):
+    xlist += [float(eval(input("The next x is:")))]
+    b += 1
+max_x = round(opt.fminbound(lambda x: -cos(x), xlist[0], xlist[degree]), 2)
+print("The maximum of %d th derivative is %.2f " % (degree+1, max_x))
+
+error_bound = (cos(max_x))/(math.factorial(degree+1))
+for a in xlist:
+    error_bound *= (xp-a)
+
+print("The error bound is equal to or smaller than %.5f" % abs(error_bound))
