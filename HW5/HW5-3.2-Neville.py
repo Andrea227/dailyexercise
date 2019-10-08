952c1e7cbae8ab6044883dc76aea6c3ec81906b0
Andrea227 <andreayangsh@outlook.com>
2019-10-08 17:59:01 +0800

    hw5-3.1.3a-2

---
+++ HW5/HW5-3.2-Neville.py
@@ -0,0 +1,34 @@
+import numpy as np
+import mathpy
+
+xp = eval(input("What's your approximation point: "))
+degree = int(input("How many data points do you have: "))
+
+xlist = np.zeros(degree)
+ylist = np.zeros(degree)
+b = 0
+while b < int(degree):
+    xlist[b] = eval(input("The next x is:"))
+    b += 1
+a = 0
+while a < int(degree):
+    ylist[a] = eval(input("The next y is:"))
+    a += 1
+
+
+def neville(x, y, x0):
+    n = x.size
+    q = np.zeros((n, n - 1))
+    # Insert 'y' in the first column of the matrix 'q'
+    q = np.concatenate((y[:, None], q), axis=1)
+
+    for i in range(1, n):
+        for j in range(1, i + 1):
+            q[i, j] = ((x0 - x[i - j]) * q[i, j - 1] - (x0 - x[i]) * q[i - 1, j - 1]) / (x[i] - x[i - j])
+    yp = q[n - 1, n - 1]
+    return [yp, q]
+
+
+print(neville(xlist, ylist, xp))
+print(mathpy.numerical.neville(xlist, ylist, xp))
+
