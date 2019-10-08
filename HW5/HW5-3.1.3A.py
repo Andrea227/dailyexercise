cc2c09f539183bfd0c7a532a1d15071082f2ef5a
Andrea227 <andreayangsh@outlook.com>
2019-10-08 10:27:06 +0800

    hw5-3.1.3a-2

--- HW5/HW5-3.1.3A.py
+++ HW5/HW5-3.1.3A.py
@@ -1,7 +1,7 @@
 import scipy.optimize as opt
 import scipy
 from numpy import *
-from numpy import sin, cos, exp, pi
+from numpy import sin, cos, exp, pi, log
 import math
 
 xp = float(eval(input("What's your approximation point: ")))
@@ -11,13 +11,13 @@ b = 1
 while b <= int(degree):
     xlist += [float(eval(input("The next x is:")))]
     b += 1
-max_x = opt.fminbound(lambda x: -abs((exp(-2 * x)) * (46 * sin(3 * x) + 9 * cos(3 * x))), xlist[0], xlist[degree])
-max_x1 = opt.fminbound(lambda x: -abs(x * (x - (pi / 6)) * (x - (pi / 4))), xlist[0], xlist[degree])
+max_x = opt.fminbound(lambda x: -abs(2/((x**3)*(log(10)))), xlist[0], xlist[degree])
+max_x1 = opt.fminbound(lambda x: -abs((x-3.0)*(x-3.2)*(x-3.5)), xlist[0], xlist[degree])
 
 print("The maximum of %d th derivative is %.2f " % (degree + 1, max_x))
 
-error_bound = (abs((exp(-2 * max_x)) * (46 * sin(3 * max_x) + 9 * cos(3 * max_x)))) / (math.factorial(degree + 1))
+error_bound = (abs(2/((max_x**3)*(log(10))))) / (math.factorial(degree + 1))
 for a in xlist:
     error_bound *= (max_x1 - a)
 
-print("The error bound is equal to or smaller than %.5f" % abs(error_bound))
+print("The error bound is equal to or smaller than %.9f" % abs(error_bound))
