988eaac0225b42a77610cedfe661fc6df3db7dc5
Andrea227 <andreayangsh@outlook.com>
2019-10-20 15:10:48 +0800

    hw6-2

---
+++ HW6/HW6-Backward.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+n = int(input("how many data points do you have: "))
+xp = float(eval(input("Your evaluation point is: ")))
+x1 = []
+y1 = []
+for i in range(n):
+    x1 += [float(input("Your next x is: "))]
+for j in range(n):
+    y1 += [float(input("Your corresponding y is: "))]
+
+Dy = np.zeros((n, n))
+Dy[:, 0] = y1
+for k in range(1, n):
+    for l in range(n-1, k-1, -1):
+        Dy[l][k] = Dy[l][k - 1] - Dy[l-1][k - 1]
+
+print(Dy)
+
+h = (x1[-1] - x1[0]) / (n - 1)
+
+
+def combup(n, r):
+    demo = n
+    i = 1
+    while i < r:
+        demo *= (n+i)
+        i += 1
+    else:
+        return demo
+
+
+def factor(x):
+    a = 1
+    if x!= 1:
+        i = 2
+        while i <= x:
+            a *= i
+            i += 1
+        else:
+            return a
+    else:
+        return a
+
+
+def backward(u):
+    s = (u - x1[4]) / h
+    sum = Dy[n-1][0]
+    for i in range(1, n):
+        sum = sum + (combup(s, i) * Dy[n-1][i]) / factor(i)
+        print(sum)
+    return print("The final evaluation is %.8f" % sum)
+
+
+backward(xp)
