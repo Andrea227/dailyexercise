cb18db76d7cfe4e12dadb206b3e03cac3b1633cf
Andrea227 <andreayangsh@outlook.com>
2019-10-21 19:20:49 +0800

    3.4.5a(rounding)

--- 3.3-excercise.py
+++ 3.3-excercise.py
@@ -1,4 +1,27 @@
-a=1.342167
+import numpy as np
 
-b = round(a, ndigits=5)
+q = np.zeros((4, 4))
+
+i = 0
+for i in range(0, 4):
+    for j in range(0, 4):
+        q[i][j] = 4 * i - j
+
+print(q)
+
+z = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def hermite(x):
+    result = q[0][0]
+    for p in range(1, 2 * 2):
+        a = 1
+        for r in range(0, p):
+            a *= (x-z[r])
+        result = result + q[p][p] * a
+    return result
+
+
+b = hermite(7)
+
 print(b)
--- HW6/HW6-Hermite.py
+++ HW6/HW6-Hermite.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 num = int(input("how many data points do you have: "))
+xp = float(input("What's your approximation: "))
 n = num - 1
 x1 = []
 y0 = []
@@ -12,31 +13,41 @@ for j in range(num):
 for k in range(num):
     y1 += [float(input("Your corresponding y1 is: "))]
 
-
-z = [0]*(2*num)
-q = np.zeros((2*num, 2*num))
+z = [0] * (2 * num)
+q = np.zeros((2 * num, 2 * num))
 i = 0
 while n >= i >= 0:
-    z[2*i] = round(x1[i], ndigits=5)
-    z[2*i+1] = round(x1[i], ndigits=5)
-    q[2*i+1][0] = round(y0[i], ndigits=5)
-    q[2*i][0] = round(y0[i], ndigits=5)
-    q[2*i+1][1] = round(y1[i], ndigits=5)
+    z[2 * i] = round(x1[i], ndigits=5)
+    z[2 * i + 1] = round(x1[i], ndigits=5)
+    q[2 * i + 1][0] = round(y0[i], ndigits=5)
+    q[2 * i][0] = round(y0[i], ndigits=5)
+    q[2 * i + 1][1] = round(y1[i], ndigits=5)
     if i != 0:
-        q[2*i][1] = round((q[2*i][0] - q[2*i-1][0])/(z[2*i] - z[2*i-1]), ndigits=5)
+        q[2 * i][1] = round((q[2 * i][0] - q[2 * i - 1][0]) / (z[2 * i] - z[2 * i - 1]), ndigits=5)
     i += 1
 
 k = 2
-while 2*n+1 >= k >= 2:
+while 2 * n + 1 >= k >= 2:
     j = 2
     while k >= j >= 2:
-        q[k][j] = round((q[k][j-1] - q[k-1][j-1])/(z[k] - z[k-j]), ndigits=5)
+        q[k][j] = round((q[k][j - 1] - q[k - 1][j - 1]) / (z[k] - z[k - j]), ndigits=5)
         j += 1
     k += 1
 
+
+def hermite(x):
+    result = q[0][0]
+    for p in range(1, 2 * num):
+        b = 1
+        for r in range(0, p):
+            b *= (x - z[r])
+        result = result + q[p][p] * b
+    return result
+
+
+a = hermite(xp)
+
 np.set_printoptions(precision=5, suppress=True, floatmode='fixed')
-print(x1)
-print(y0)
-print(y1)
-print(z)
+print("The difference table is")
 print(q)
+print("The evaluation at %.5f is %.5f" % (xp, a))
